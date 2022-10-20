ebfdb8d6fa39b3894c2781c60c67b7274a146ad8
jyc-coder <jyc4648@gmail.com>
2022-10-20 11:32:30 +0900

    #2 - 네비게이션 바 구현
    검색 결과를 항목으로 나누어서 사용자에게 보여줄수 있게 네비게이션 바를 구현함
    
    * `NavLink`를 사용해서 `isActive`여부에 따라 텍스트의 색이 변경되게 해서 어느 항목의 검색 결과를 보여주는지를 알기 쉽게 설정함
    
    * `App`에다가 `Routes`를 작성해서 `/`경로로 들어가도 초기 상태에는 무조건 `/all`로 리다이렉트하게 설정함으로써, 다른 항목을 선택하지 않은 경우에는 항상 통합 검색 값을 표시하게 설정함

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import NavigationBar from './components/NavigationBar';
 import SearchInput from './components/SearchInput';
 
 function App() {
   return (
     <div className='bg-slate-50 min-h-screen'>
       <div className='flex flex-wrap justify-center relative'>
-        <SearchInput/>
+        <SearchInput />
+        <NavigationBar/>
         {/* navigation */}
       </div>
+      <Routes>
+        <Route exact path="/" element={<Navigate to="/all"/>}/>
+      </Routes>
       {/* 검색 결과 */}
     </div>
   );
