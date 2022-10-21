08dc389489b677c121aa512aa216405b9ee30ee9
jyc-coder <jyc4648@gmail.com>
2022-10-21 15:57:57 +0900

    #2 - 통합 검색 결과 구현
    ! 원래 계획은 google search API를 사용하여 구현하려고 했으나 API가 사라져서 대신 Bing search API를 사용함
    
    https://rapidapi.com/microsoft-azure-org-microsoft-cognitive-services/api/bing-web-search1
    
    * API 키 값은 환경변수로 따로 선언하여 보안상 보이지 않게 작업함 ( 만약 누군가가 이걸 사용한다면 API 키 값은 따로 본인이 얻어서 작성할 것!)
    
    * `SearchInput`컴포넌트에서 입력한 값을 쿼리로 추가하고  해당 쿼리 값을 통해서 API의 GET메소드를 호출하여 받은 데이터들을 반복적으로 렌더링함
    
    * `react-query`에서 `useQuery`를 사용하여 `isLoading`이 참일 경우에 `react-loader-spinner`에서 가져온 `Oval`이 나타나게 설정함
    
    * `react-paginate`를 사용해서 10개의 데이터만 나타나게 하고 밑에 페이지를 선택하는 컴포넌트를 제작함(`itemsPerPage`의 값을 10으로 지정함)
    
    * 만약 검색어가 입력되지 않은 경우에는 `SearchTermRequired`컴포넌트가 렌더링 되고, 결과값이 존재하지 않을때는 `NoResult`값이 렌더링 됨
    
    * 페이지네이션의 디자인은 `tailwindcss`를 사용하여 설정함

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import NavigationBar from './components/NavigationBar';
 import SearchInput from './components/SearchInput';
+import AllResult from './components/results/AllResult';
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
         {/* navigation */}
       </div>
       <Routes>
-        <Route exact path="/" element={<Navigate to="/all"/>}/>
+        <Route exact path="/" element={<Navigate to="/all" />} />
+        <Route exact path="/all" element={<AllResult/>}/>
       </Routes>
       {/* 검색 결과 */}
     </div>
